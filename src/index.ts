1785239165dbe85e0682ccf052c8b21ab7dba0b1
𝑐æ𝑙𝑜𝜅’’ <107492325+caelok@users.noreply.github.com>
2025-05-21 17:15:35 +0200

    reformatting

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export { SwoshDb } from './db';
 export { Collection, Document } from './collection';
-export { Query } from './query';
 export { StorageEngine } from './storage/engine';
 export { FileStorage } from './storage/file';
+export { Query } from './query';
+export { SwoshDb } from './db';
